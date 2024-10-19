4a04ec06c82a966a14c1156078b8e50bb2e9f308
platane <me@platane.me>
2024-10-19 19:02:44 +0900

    .

--- packages/webRTC/signal.tsx
+++ packages/webRTC/signal.tsx
@@ -1,6 +1,5 @@
 // const STORE_URL = "https://bong-bong--webrtc-signal.platane.workers.dev";
-// const STORE_URL = location.origin + "/signal";
-const STORE_URL = "https://bongbong-ck3.pages.dev/signal";
+const STORE_URL = location.origin + "/signal";
 
 export const signalBroadcast = async <D,>(
   key: string,
