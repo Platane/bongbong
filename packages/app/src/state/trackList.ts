ed26b8f32c55d94cd2c694565f0e29edc0ee8fbe
platane <me@platane.me>
2024-11-21 08:36:04 +0545

    .

--- packages/app/src/state/trackList.ts
+++ packages/app/src/state/trackList.ts
@@ -3,12 +3,6 @@ import type { Note, Partition } from "../state/game";
 // @ts-ignore
 import track1_url from "../asset/Different Heaven & EH!DE - My Heart.m4a";
 
-// @ts-ignore
-import track2_url from "../asset/DEAF KEV - Invincible.m4a";
-
-// @ts-ignore
-import track3_url from "../asset/DragonForce - Through the Fire and Flames.m4a";
-
 const toNotes = (
   sequence: string,
   { bpm, offset = 0 }: { bpm: number; offset?: number }
@@ -42,13 +36,15 @@ export const tracks = [
   {
     title: "Different Heaven & EH!DE - My Heart",
     src: track1_url,
+    // src: "https://github.com/user-attachments/assets/06c4649f-1e4f-4816-9e99-87717bbf7b00",
     ...toNotes(
       `
       - - - - | S - - - | S - - - | S - - - |
       S - - - | S - - - | S - - - | S - R R | 
       S - - - | - - - - | - - - - | - - - - |
-      - - - - | - - - - | - - - - | - - - - |
+      - - - - | R R R R | - S - S | - - - - |
       S - - S | S - S - | R R R R | - - - - |
+      - - - - | - - - - | - - - - | - - - - |
       
       `,
       { bpm: 174, offset: 0 }
@@ -56,12 +52,12 @@ export const tracks = [
   },
   {
     title: "DEAF KEV - Invincible.m4a",
-    src: track2_url,
+    src: "https://github.com/user-attachments/assets/ac0c3def-221d-431e-b59d-5887c6d22d79",
     ...toNotes("", { bpm: 1, offset: 0 }),
   },
   {
     title: "DragonForce - Through the Fire and Flames",
-    src: track3_url,
+    src: "https://github.com/user-attachments/assets/d6d1d7cb-528c-4072-bec9-de64767963ce",
     ...toNotes(
       `
       S - - - |
