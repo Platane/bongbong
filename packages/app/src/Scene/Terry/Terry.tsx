4a9b91e28594324c8f7d29b32d1ed68c90dea2ae
platane <me@platane.me>
2024-12-17 15:21:11 +0700

    skinned drum

--- packages/app/src/Scene/Terry/Terry.tsx
+++ packages/app/src/Scene/Terry/Terry.tsx
@@ -1,5 +1,5 @@
-import { useFrame } from "@react-three/fiber";
-import React from "react";
+import { useFrame, useThree } from "@react-three/fiber";
+import React, { useRef } from "react";
 import { MeshToonMaterial } from "three";
 import * as THREE from "three";
 import { Face } from "./Face";
@@ -31,7 +31,7 @@ export const Terry = ({
       (["uwu", "openMouth", "mischief"] as const)[
         Math.floor(Math.random() * 3)
       ],
-    "uwu"
+    "uwu",
   );
 
   return (
@@ -46,7 +46,7 @@ export const Terry = ({
 
         setRandomStance();
         setB(
-          new THREE.Vector3(1 + Math.cos(angle) * 2, 0, Math.sin(angle) * 2)
+          new THREE.Vector3(1 + Math.cos(angle) * 2, 0, Math.sin(angle) * 2),
         );
       }}
     >
@@ -57,7 +57,7 @@ export const Terry = ({
         <Body />
         <Face
           rotation={[-Math.PI / 2, 0, 0]}
-          position={[0, 1, 0]}
+          position={[0, 1.02, 0]}
           stance={stance}
         />
         <Arm
@@ -87,13 +87,112 @@ export const Terry = ({
 };
 
 const Body = () => {
+  const bones = React.useMemo(createDrumBones, []);
+
+  const ref = useRef<THREE.SkinnedMesh | null>(null);
+
+  const { scene } = useThree();
+
+  React.useEffect(() => {
+    const mesh = ref.current;
+
+    if (!mesh) return;
+
+    mesh.add(bones.root);
+    mesh.bind(bones.skeletton);
+
+    const helper = new THREE.SkeletonHelper(mesh);
+    scene.add(helper);
+  }, []);
+
+  useFrame(() => {
+    const k = (Math.sin(Date.now() * 0.01) + 1) * 0.1;
+    bones.update(k);
+  });
+
   return (
-    <mesh geometry={drumGeometry}>
+    <skinnedMesh geometry={drumGeometry} ref={ref}>
       {/* <meshToonMaterial color={"blue"} /> */}
       {/* <meshStandardMaterial color="blue" /> */}
       <meshNormalMaterial />
-    </mesh>
+    </skinnedMesh>
+  );
+};
+
+const createDrumBones = () => {
+  const root = new THREE.Bone();
+  root.position.set(0, 0, 0);
+
+  const head = new THREE.Bone();
+  head.position.set(0, 1, 0);
+  root.add(head);
+
+  const ass = new THREE.Bone();
+  ass.position.set(0, -1, 0);
+  root.add(ass);
+
+  const bones = [root, head, ass];
+  const skeletton = new THREE.Skeleton(bones);
+
+  const update = (curve: number) => {
+    head.position.set(0, 1, 0);
+    head.quaternion.identity();
+    head.rotateZ(curve * 4);
+  };
+
+  return { root, skeletton, bones, update };
+};
+
+const computeDrumWeight = (
+  geometry: THREE.BufferGeometry,
+  bones: THREE.Bone[],
+) => {
+  const positions = geometry.getAttribute("position")!;
+
+  const skinIndices = Array.from({ length: positions.count }, () => [
+    0, 0, 0, 0,
+  ]).flat();
+  const skinWeights = Array.from({ length: positions.count }, () => [
+    1, 0, 0, 0,
+  ]).flat();
+
+  const p = new THREE.Vector3();
+  const b = new THREE.Vector3();
+  const weights = Array.from(bones, () => 0);
+
+  for (let i = positions.count; i--; ) {
+    p.set(positions.getX(i), positions.getY(i), positions.getZ(i));
+
+    for (let j = bones.length; j--; ) {
+      bones[j].getWorldPosition(b);
+
+      const d = p.distanceTo(b);
+      const w = 1 / d ** 3;
+
+      weights[j] = w;
+    }
+
+    const bs = weights
+      .map((weight, index) => ({ weight, index }))
+      .sort((a, b) => a.weight - b.weight)
+      .slice(0, 4);
+
+    const sum = bs.reduce((sum, { weight }) => sum + weight, 0);
+    for (let k = 4; k--; ) {
+      skinIndices[i * 4 + k] = bs[k]?.index ?? 0;
+      skinWeights[i * 4 + k] = (bs[k]?.weight ?? 0) / sum;
+    }
+  }
+
+  geometry.setAttribute(
+    "skinIndex",
+    new THREE.Uint16BufferAttribute(skinIndices, 4),
+  );
+  geometry.setAttribute(
+    "skinWeight",
+    new THREE.Float32BufferAttribute(skinWeights, 4),
   );
 };
 
 const drumGeometry = createDrumGeometry();
+computeDrumWeight(drumGeometry, createDrumBones().bones);
